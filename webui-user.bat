e366a6b224007605f6f696c5656e65724c3f5276
Ian <o10baird@gmail.com>
2023-02-20 11:31:56 -0500

    Update WebUI for Dependencies

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --share --gradio-auth scbff:testDN --xformers
+set COMMANDLINE_ARGS=--listen --share --gradio-auth scbff:testDN --xformers --reinstall-torch --reinstall-xformers
 
 call webui.bat
